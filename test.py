6d422e66369b50d02793fece8cf043ed16bba8b2
ungezief3r <bjh4191@outlook.com>
2026-02-23 20:37:47 +0900

    v1.1.0 -곱셈공식 (a+b)^3, (a+b+c)(a^2+b^2+c^2-ab-bc-ca)추가 -valueError 예외처리 추가

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+#곱셈공식 (x+a)(x+b)(x+c)
+
 def formula_1():
     a = int(input("a: "))
     b = int(input("b: "))
@@ -6,6 +8,9 @@ def formula_1():
     result = (x**3 + (a + b + c) * x**2 + (a*b + b*c + a*c) * x + (a*b*c))
     return result
 
+
+#곱셈공식 (a+b+c)^2
+
 def formula_2():
     a = int(input("a: "))
     b = int(input("b: "))
@@ -13,12 +18,35 @@ def formula_2():
     result = ((a**2 + b**2 + c**2) + 2*(a*b + b*c + a*c))
     return result
 
-formulas = [formula_1, formula_2]
+
+#곱셈공식 (a+b)^3
+
+def formula_3():
+    a = int(input("a: "))
+    b = int(input("b: "))
+    result = ((a**3 + 3*a**2*b + 3*a*b**2 + b**3))
+    return result
+
+
+#곱셈공식 (a+b+c)(a^2+b^2+c^2-ab-bc-ca)
+
+def formula_4():
+    a = int(input("a: "))
+    b = int(input("b: "))
+    c = int(input("c: "))
+    result = ((a**3 + b**3 + c**3) - 3*a*b*c)
+    return result
+
+
+#결정
+
+formulas = [formula_1, formula_2, formula_3, formula_4]
 
 while True:
-    decide = int(input("곱셈공식: "))
-    if decide <=2:
+    try:
+        decide = int(input("곱셈공식: "))
         print(formulas[int(decide)-1]())
-    else:
+        
+    except ValueError:
         print("null")
           
