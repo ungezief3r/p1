2de8e3872ffbe7596ab284f3ad590e09d6d36084
ungezief3r <bjh4191@outlook.com>
2026-02-24 10:17:04 +0900

    v1.2.2
    - 곱셈공식 5번 공식 추가
    - 방정식 기능 추가
    - 근의 공식 추가
    - 전체 로직 리팩토링

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 class calculator:
     def __init__(self):
-        self.formulas = [self.formula_1, self.formula_2, self.formula_3, self.formula_4]
+        
+        self.formulas = [self.formula_1, self.formula_2, self.formula_3, self.formula_4, self.formula_5]
     
-
 
     #곱셈공식 (x+a)(x+b)(x+c)
     def formula_1(self):
@@ -13,7 +13,6 @@ class calculator:
         result = (x**3 + (a + b + c) * x**2 + (a*b + b*c + a*c) * x + (a*b*c))
         return result
 
-
     #곱셈공식 (a+b+c)^2
     def formula_2(self):
         a = int(input("a: "))
@@ -22,14 +21,12 @@ class calculator:
         result = ((a**2 + b**2 + c**2) + 2*(a*b + b*c + a*c))
         return result
 
-
     #곱셈공식 (a+b)^3
     def formula_3(self):
         a = int(input("a: "))
         b = int(input("b: "))
         result = ((a**3 + 3*a**2*b + 3*a*b**2 + b**3))
         return result
-
 
     #곱셈공식 (a+b+c)(a^2+b^2+c^2-ab-bc-ca)
     def formula_4(self):
@@ -39,17 +36,92 @@ class calculator:
         result = ((a**3 + b**3 + c**3) - 3*a*b*c)
         return result
 
+    #곱셈공식 (a+b)(a^2-ab+b^2)
+    def formula_5(self):
+        a = int(input("a: "))
+        b = int(input("b: "))
+        result = ((a**3 + b**3))
+        return result
 
-#결정
+
+    #결정
 
     def run(self):
-        while True:
-            try:
+        
+        try:
                 decide = int(input("곱셈공식: "))
-                print(self.formulas[int(decide)-1]())
+                print(self.formulas[(decide)-1]())
                 
-            except:
+        except ValueError:
                 print("null")
+            
+        except IndexError:
+                print("null")
+            
+
+class equations:
+
+
+    def __init__(self):
+        self.equations = [self.linear_equation,self.quadratic_equation]
+
+    def quadratic_equation(self):
+        a = int(input("a: "))
+        b = int(input("b: "))
+        c = int(input("c: "))
+        d = (b**2 - 4*a*c)
+
+        if d > 0:
+
+            plus = ((-b + d**0.5)/(2*a))
+            minus = ((-b - d**0.5)/(2*a))
+
+            result("x1 = " + str(plus) + "x2 = " + str(minus))
+            return result
+                    
+        elif d == 0:
+            result = (0)
+            return result
+
+        elif d < 0:
+            result = ("허근")
+            return result
+            
+
+    def linear_equation(self):
+        a = int(input("x의 계수: "))
+        b = int(input("상수항: "))
+        result = (-b/a)
+
+        return result
+    
+    def run(self):
+        try:
+            self.decide = int(input("차수: "))
+            target_formula = (self.equations[(self.decide)-1])
                 
+            print(target_formula())
+            
+        except IndexError:
+                print("null")
+
+        except ValueError:
+                print("null")
+
+                
+
+equa = equations()
 calc = calculator()
-calc.run()
+
+calculator_modes = [equa, calc]
+
+while True:
+    try:
+        decide = int(input("방정식=1, 곱셈공식=2: "))
+        selected = calculator_modes[(decide)-1]
+        selected.run()
+    
+    except ValueError:
+        print("null")
+    except IndexError:
+        print("null")
